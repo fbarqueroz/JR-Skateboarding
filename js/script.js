6a488945794d2ca2c46c4d801dc9ed254a4c65fb
Alex D. Guzman Ramirez <alexd.guzman98@gmail.com>
2021-03-09 23:31:10 -0600

    Fix Js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 // Sidebar Toggle
 const btnToggle = document.querySelector('.toggle-btn');
-btnToggle.addEventListener('click', function () {
+btnToggle.addEventListener('click', (e) => {
+  e.preventDefault();
   document.getElementById('sidebar').classList.toggle('active');
 });
 // Modal
 const openModal = document.getElementById('openModal');
-openModal.addEventListener('click',(e) => {
- e.preventDefault();
-  document.getElementById('vent').style.display='block';
-  document.getElementById('overlay').style.display='block';
+openModal.addEventListener('click', (e) => {
+  e.preventDefault();
+  document.getElementById('vent').style.display = 'block';
+  document.getElementById('overlay').style.display = 'block';
 });
 const closeModal = document.getElementById('closeModal');
-closeModal.addEventListener('click',(e) => {
+closeModal.addEventListener('click', (e) => {
   e.preventDefault();
-  document.getElementById('vent').style.display='none';
-  document.getElementById('overlay').style.display='none';
+  document.getElementById('vent').style.display = 'none';
+  document.getElementById('overlay').style.display = 'none';
 });
- 
-
