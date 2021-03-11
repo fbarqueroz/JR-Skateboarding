8d1783f7ff761f4a5a6c5a3dd9cefd881fe52c27
Alex D. Guzman Ramirez <alexd.guzman98@gmail.com>
2021-03-11 16:06:33 -0600

    Fix

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
 // Sidebar Toggle
 const btnToggle = document.querySelector('.toggle-btn');
-btnToggle.addEventListener('click', (e) => {
-  e.preventDefault();
+btnToggle.addEventListener('click', () => {
   document.getElementById('sidebar').classList.toggle('active');
 });
 // Modal
 const openModal = document.getElementById('openModal');
-openModal.addEventListener('click', (e) => {
-  e.preventDefault();
+openModal.addEventListener('click', () => {
   document.getElementById('vent').style.display = 'block';
   document.getElementById('overlay').style.display = 'block';
 });
 const closeModal = document.getElementById('closeModal');
-closeModal.addEventListener('click', (e) => {
-  e.preventDefault();
+closeModal.addEventListener('click', () => {
   document.getElementById('vent').style.display = 'none';
   document.getElementById('overlay').style.display = 'none';
 });
