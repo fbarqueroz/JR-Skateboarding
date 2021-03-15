b01fd3c3456f75bff8cca0570202f83015c4832c
Fabian Barquero <fabianbarquero9@gmail.com>
2021-03-15 17:07:34 -0600

    Add tabs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-plusplus */
 // Sidebar Toggle
 const btnToggle = document.querySelector('.toggle-btn');
 btnToggle.addEventListener('click', () => {
@@ -15,3 +17,31 @@ closeModal.addEventListener('click', () => {
   document.getElementById('overlay').style.display = 'none';
 });
 // Slider
+
+// Tabs
+const enlaces = document.querySelectorAll('.tabs-tablist-item');
+const contentido = document.querySelectorAll('.tabs-content-item');
+for (let i = 0; i < contentido.length; i++) {
+  contentido[i].classList.add('js-content-hidden');
+}
+
+contentido[0].classList.remove('js-content-hidden');
+
+enlaces[0].classList.remove('js-active');
+
+for (let i = 0; i < enlaces.length; i++) {
+  enlaces[i].addEventListener('click', (event) => {
+    const id = event.currentTarget.getAttribute('href').substring(1);
+
+    for (let i = 0; i < contentido.length; i++) {
+      contentido[i].classList.add('js-content-hidden');
+      if (contentido[i].getAttribute('id') === id) {
+        contentido[i].classList.remove('js-content-hidden');
+      }
+    }
+    for (let i = 0; i < enlaces.length; i++) {
+      enlaces[i].classList.remove('js-active');
+    }
+    event.currentTarget.classList.add('js-active');
+  });
+}
