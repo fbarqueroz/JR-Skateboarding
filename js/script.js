c45eaf373f19eeece28ec9e22d7288bcd859a0e7
Alex D. Guzman Ramirez <66041707+AlexGuzman98@users.noreply.github.com>
2021-03-22 17:31:55 -0600

    fix

--- js/script.js
+++ js/script.js
@@ -3,3 +3,35 @@ const btnToggle = document.querySelector('.toggle-btn');
 btnToggle.addEventListener('click', () => {
   document.getElementById('sidebar').classList.toggle('active');
 });
+  
+// slider
+let indice = 1;
+let i = '';
+function muestraSlides(n) {
+  const slides = document.getElementsByClassName('miSlider');
+  if (n > slides.length) {
+    indice = 1;
+  }
+  if (n < 1) {
+    indice = slides.length;
+  }
+  for (i = 0; i < slides.length; i += 1) {
+    slides[i].style.display = 'none';
+  }
+  slides[indice - 1].style.display = 'block';
+}
+function avanzaSlide() {
+  muestraSlides(indice += 1);
+}
+const avanzar = document.getElementById('avanzar');
+avanzar.addEventListener('click', () => {
+  avanzaSlide();
+});
+function retrocederSlide() {
+  muestraSlides(indice -= 1);
+}
+const retroceder = document.getElementById('retroceder');
+retroceder.addEventListener('click', () => {
+  retrocederSlide();
+});
+muestraSlides(indice);
