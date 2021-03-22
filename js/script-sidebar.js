5337b3296ead16bc103760479127edf431613a69
Alex D. Guzman Ramirez <66041707+AlexGuzman98@users.noreply.github.com>
2021-03-22 16:35:13 -0600

    Inicio de HomePage

--- js/script-sidebar.js
+++
@@ -1,5 +0,0 @@
-// Sidebar Toggle
-const btnToggle = document.querySelector('.toggle-btn');
-btnToggle.addEventListener('click', () => {
-  document.getElementById('sidebar').classList.toggle('active');
-});
