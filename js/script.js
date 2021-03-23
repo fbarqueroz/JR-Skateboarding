139e894b6ec05c95cda412e33c2bba84b4451a14
Alex D. Guzman Ramirez <66041707+AlexGuzman98@users.noreply.github.com>
2021-03-23 17:01:35 -0600

    FIX

--- js/script.js
+++ js/script.js
@@ -4,22 +4,21 @@ btnToggle.addEventListener('click', () => {
   document.getElementById('sidebar').classList.toggle('active');
 });
 
-// Slider
+// Slider-decks
 let indice = 1;
 let i = '';
 function muestraSlides(n) {
   const slides = document.getElementsByClassName('miSlider');
-  const slideSoon = document.getElementsByClassName('slider-soon');
-  if (n > (slideSoon, slides).length) {
+  if (n > slides.length) {
     indice = 1;
   }
   if (n < 1) {
-    indice = (slideSoon, slides).length;
+    indice = slides.length;
   }
-  for (i = 0; i < (slideSoon, slides).length; i += 1) {
-    (slideSoon[i], slides[i]).style.display = 'none';
+  for (i = 0; i < slides.length; i += 1) {
+    slides[i].style.display = 'none';
   }
-  (slideSoon[indice - 1], slides[indice - 1]).style.display = 'block';
+  slides[indice - 1].style.display = 'block';
 }
 function avanzaSlide() {
   muestraSlides(indice += 1);
@@ -36,3 +35,35 @@ retroceder.addEventListener('click', () => {
   retrocederSlide();
 });
 muestraSlides(indice);
+
+// // Slider-soon
+// let indice = 1;
+// let i = '';
+// function muestraSlides(n) {
+//   const slides = document.getElementsByClassName('slider-soon');
+//   if (n > slides.length) {
+//     indice = 1;
+//   }
+//   if (n < 1) {
+//     indice = slides.length;
+//   }
+//   for (i = 0; i < slides.length; i += 1) {
+//     slides[i].style.display = 'none';
+//   }
+//   slides[indice - 1].style.display = 'block';
+// }
+// function avanzaSlide() {
+//   muestraSlides(indice += 1);
+// }
+// const avanzar = document.getElementById('avanzar');
+// avanzar.addEventListener('click', () => {
+//   avanzaSlide();
+// });
+// function retrocederSlide() {
+//   muestraSlides(indice -= 1);
+// }
+// const retroceder = document.getElementById('retroceder');
+// retroceder.addEventListener('click', () => {
+//   retrocederSlide();
+// });
+// muestraSlides(indice);
