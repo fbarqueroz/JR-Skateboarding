ebf9025cec2dac6441506e45549bf38918fd250e
Alex Daniel Guzmán Ramírez <a.guzman@WEB-6.local>
2021-04-06 13:18:21 -0600

    modal user

--- js/script-modal.js
+++ js/script-modal.js
@@ -1,7 +1,7 @@
 // Products
-const modal = document.querySelector('.modal');
-const flex = document.querySelector('.flex');
-const abrir = document.querySelector('.open');
+const modal = document.getElementById('miModal');
+const flex = document.getElementById('flex');
+const abrir = document.getElementById('abrir');
 const cerrar = document.getElementById('close');
 
 abrir.addEventListener('click', (e) => {
@@ -18,19 +18,3 @@ window.addEventListener('click', (e) => {
     modal.style.display = 'none';
   }
 });
-fetch('https://604ab2419251e100177cf001.mockapi.io/Decks')
-  .then((response) => response.json())
-  .then((data) => {
-    const modalText = document.querySelector('.modal__text--decks');
-    for (let i = 0; i < data.length; i += 1) {
-      modalText.innerHTML = `
-              <div >
-                <img src="${data[i].img}" alt=""><br>
-                <a href = '#'${data[i].name}">${data[i].name}</a>
-                <p>${data[i].currency}${data[i].price}</p>
-                <p>Name: ${data[i].name}</p>
-                <p>Description: ${data[i].description}</p>
-              </div>
-        `;
-    }
-  });
